b892b31f7f9bc9a63eb9774e7fbe0b5cd82891ae
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-12-15 17:06:05 -0500

    Update precompile addresses

--- contracts/nativeAssets.sol
+++ contracts/nativeAssets.sol
@@ -3,8 +3,8 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 library NativeAssets {
-    address constant balanceAddr = 0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF;
-    address constant transferAddr = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
+    address constant balanceAddr = 0x0100000000000000000000000000000000000001;
+    address constant transferAddr = 0x0100000000000000000000000000000000000002;
 
     function assetBalance(address addr, uint256 assetID) public returns (uint256) {
         (bool success, bytes memory data) = balanceAddr.call(abi.encodePacked(addr, assetID));
