d30aa21e91f92c4a7e0f0222e101d3d805345e2a
Determinant <tederminant@gmail.com>
2021-02-23 18:27:37 -0500

    forgot to add the download command

--- examples/cedrusdb/build.sh
+++ examples/cedrusdb/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+go get -d github.com/Determinant/cedrusdb-go
 source "$(go env GOPATH)/pkg/mod/github.com/!determinant/cedrusdb-go@$(go list -m github.com/Determinant/cedrusdb-go | awk '{print $2}')/scripts/env.sh" && go build -o payment ./*.go
