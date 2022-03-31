78a5c1774a79766dd77d012af5bc7e4af1b15f3e
Aaron Buchwald <aaron.buchwald56@gmail.com>
2022-03-31 15:06:03 +0200

    Move fill trie helpers and add error var to leaf syncer

--- sync/statesync/sync_test.go
+++ sync/statesync/sync_test.go
@@ -7,13 +7,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"math/big"
 	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/ava-labs/avalanchego/utils/wrappers"
 
 	"github.com/ava-labs/coreth/core/rawdb"
 	"github.com/ava-labs/coreth/core/state/snapshot"
@@ -84,14 +81,14 @@ func TestSyncer(t *testing.T) {
 				serverTrieDB := trie.NewDatabase(memorydb.New())
 				return serverTrieDB, common.BytesToHash([]byte("totally-fake-root")), nil
 			},
-			expectedError: errors.New("failed to fetch leafs"),
+			expectedError: statesyncclient.ErrFailedToFetchLeafs,
 		},
 		{
 			name: "empty_server_trie",
 			prepareForTest: func(t *testing.T) (*trie.Database, common.Hash, []common.Hash) {
 				return trie.NewDatabase(memorydb.New()), common.BytesToHash([]byte("some empty bytes")), nil
 			},
-			expectedError: errors.New("failed to fetch leafs"),
+			expectedError: statesyncclient.ErrFailedToFetchLeafs,
 		},
 		{
 			name: "inconsistent_server_trie",
@@ -115,7 +112,7 @@ func TestSyncer(t *testing.T) {
 				}
 				return serverTrieDB, root, nil
 			},
-			expectedError: errors.New("failed to fetch leafs"),
+			expectedError: statesyncclient.ErrFailedToFetchLeafs,
 		},
 		{
 			name: "sync_non_latest_root",
@@ -222,7 +219,7 @@ func TestSyncer(t *testing.T) {
 				})
 				return serverTrieDB, root, nil
 			},
-			expectedError: errors.New("failed to fetch leafs"),
+			expectedError: statesyncclient.ErrFailedToFetchLeafs,
 		},
 		{
 			name: "accounts_with_missing_code",
@@ -306,74 +303,6 @@ func TestSyncer(t *testing.T) {
 			}
 		})
 	}
-}
-
-func fillAccountsWithStorage(t *testing.T, serverTrieDB *trie.Database, root common.Hash) common.Hash {
-	return fillAccounts(t, serverTrieDB, root, 1000, func(t *testing.T, index int64, account types.StateAccount, tr *trie.Trie) types.StateAccount {
-		if index%3 == 0 {
-			codeBytes := make([]byte, 256)
-			_, err := rand.Read(codeBytes)
-			if err != nil {
-				t.Fatalf("error reading random code bytes: %v", err)
-			}
-
-			codeHash := crypto.Keccak256Hash(codeBytes)
-			rawdb.WriteCode(serverTrieDB.DiskDB(), codeHash, codeBytes)
-			account.CodeHash = codeHash[:]
-
-			// now create state trie
-			numKeys := 16
-			account.Root, _, _ = trie.GenerateTrie(t, serverTrieDB, numKeys, wrappers.LongLen+1)
-		}
-		return account
-	})
-}
-
-func fillAccounts(
-	t *testing.T, trieDB *trie.Database, root common.Hash, accountsLen int64,
-	onAccount func(*testing.T, int64, types.StateAccount, *trie.Trie) types.StateAccount,
-) common.Hash {
-	tr, err := trie.New(root, trieDB)
-	if err != nil {
-		t.Fatalf("error opening trie: %v", err)
-	}
-	for i := int64(0); i < accountsLen; i++ {
-		acc := types.StateAccount{
-			Nonce:    uint64(i),
-			Balance:  big.NewInt(i % 1337),
-			CodeHash: types.EmptyCodeHash[:],
-			Root:     types.EmptyRootHash,
-		}
-
-		if i%5 == 0 {
-			acc.Nonce += rand.Uint64()
-		}
-
-		if onAccount != nil {
-			acc = onAccount(t, i, acc, tr)
-		}
-
-		accBytes, err := rlp.EncodeToBytes(acc)
-		if err != nil {
-			t.Fatalf("failed to rlp encode account: %v", err)
-		}
-
-		accHash := make([]byte, common.HashLength)
-		if _, err := rand.Read(accHash); err != nil {
-			t.Fatalf("error reading random bytes: %v", err)
-		}
-		if err = tr.TryUpdate(accHash, accBytes); err != nil {
-			t.Fatalf("error updating trie with account, hash=%s, err=%v", common.BytesToHash(accHash), err)
-		}
-	}
-	newRoot, _, err := tr.Commit(nil)
-	if err != nil {
-		t.Fatalf("error committing trie: %v", err)
-	}
-	if err := trieDB.Commit(newRoot, false, nil); err != nil {
-		t.Fatalf("error committing trieDB: %v", err)
-	}
-	return newRoot
 }
 
 func TestSyncerSyncsToNewRoot(t *testing.T) {
--- sync/statesync/test_sync.go
+++ sync/statesync/test_sync.go
@@ -5,8 +5,11 @@ package statesync
 
 import (
 	"bytes"
+	"math/big"
+	"math/rand"
 	"testing"
 
+	"github.com/ava-labs/avalanchego/utils/wrappers"
 	"github.com/ava-labs/coreth/core/rawdb"
 	"github.com/ava-labs/coreth/core/state/snapshot"
 	"github.com/ava-labs/coreth/core/types"
@@ -51,4 +54,72 @@ func assertDBConsistency(t testing.TB, root common.Hash, serverTrieDB, clientTri
 		})
 		return nil
 	})
+}
+
+func fillAccountsWithStorage(t *testing.T, serverTrieDB *trie.Database, root common.Hash) common.Hash {
+	return fillAccounts(t, serverTrieDB, root, 1000, func(t *testing.T, index int64, account types.StateAccount, tr *trie.Trie) types.StateAccount {
+		if index%3 == 0 {
+			codeBytes := make([]byte, 256)
+			_, err := rand.Read(codeBytes)
+			if err != nil {
+				t.Fatalf("error reading random code bytes: %v", err)
+			}
+
+			codeHash := crypto.Keccak256Hash(codeBytes)
+			rawdb.WriteCode(serverTrieDB.DiskDB(), codeHash, codeBytes)
+			account.CodeHash = codeHash[:]
+
+			// now create state trie
+			numKeys := 16
+			account.Root, _, _ = trie.GenerateTrie(t, serverTrieDB, numKeys, wrappers.LongLen+1)
+		}
+		return account
+	})
+}
+
+func fillAccounts(
+	t *testing.T, trieDB *trie.Database, root common.Hash, accountsLen int64,
+	onAccount func(*testing.T, int64, types.StateAccount, *trie.Trie) types.StateAccount,
+) common.Hash {
+	tr, err := trie.New(root, trieDB)
+	if err != nil {
+		t.Fatalf("error opening trie: %v", err)
+	}
+	for i := int64(0); i < accountsLen; i++ {
+		acc := types.StateAccount{
+			Nonce:    uint64(i),
+			Balance:  big.NewInt(i % 1337),
+			CodeHash: types.EmptyCodeHash[:],
+			Root:     types.EmptyRootHash,
+		}
+
+		if i%5 == 0 {
+			acc.Nonce += rand.Uint64()
+		}
+
+		if onAccount != nil {
+			acc = onAccount(t, i, acc, tr)
+		}
+
+		accBytes, err := rlp.EncodeToBytes(acc)
+		if err != nil {
+			t.Fatalf("failed to rlp encode account: %v", err)
+		}
+
+		accHash := make([]byte, common.HashLength)
+		if _, err := rand.Read(accHash); err != nil {
+			t.Fatalf("error reading random bytes: %v", err)
+		}
+		if err = tr.TryUpdate(accHash, accBytes); err != nil {
+			t.Fatalf("error updating trie with account, hash=%s, err=%v", common.BytesToHash(accHash), err)
+		}
+	}
+	newRoot, _, err := tr.Commit(nil)
+	if err != nil {
+		t.Fatalf("error committing trie: %v", err)
+	}
+	if err := trieDB.Commit(newRoot, false, nil); err != nil {
+		t.Fatalf("error committing trieDB: %v", err)
+	}
+	return newRoot
 }
