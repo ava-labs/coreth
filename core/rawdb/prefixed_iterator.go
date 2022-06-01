b09f6c1291f59b95395a483a3b72e41658aab3a8
Darioush Jalali <darioush.jalali@avalabs.org>
2022-06-01 09:33:02 -0700

    pr comments: iterator key len and comments

--- core/rawdb/prefixed_iterator.go
+++ core/rawdb/prefixed_iterator.go
@@ -7,30 +7,36 @@ import (
 	"bytes"
 
 	"github.com/ava-labs/coreth/ethdb"
-	"github.com/ethereum/go-ethereum/common"
 )
 
+// prefixIterator wraps an [ethdb.Iterator], returning keys that match a certain
+// prefix and key length within an optionally provided [start, end] range (both inclusive).
+// The prefix is removed from keys before returning them, and values remain untouched
 type prefixIterator struct {
 	ethdb.Iterator
 	prefixLen  int
+	keyLen     int
 	start, end []byte
 }
 
-func NewPrefixIterator(db ethdb.Iteratee, prefix []byte, start, end []byte) ethdb.Iterator {
+func NewPrefixIterator(db ethdb.Iteratee, prefix []byte, keyLen int, start, end []byte) ethdb.Iterator {
 	return &prefixIterator{
 		Iterator:  db.NewIterator(prefix, start),
 		prefixLen: len(prefix),
+		keyLen:    keyLen,
 		start:     start,
 		end:       end,
 	}
 }
 
+// Next moves the iterator to the next key matching the prefix and expected key length.
+// Returns true if there are more items to iterate.
 func (it *prefixIterator) Next() bool {
 	for it.Iterator.Next() {
-		if len(it.Iterator.Key()) != it.prefixLen+common.HashLength {
+		if len(it.Iterator.Key()) != it.prefixLen+it.keyLen {
 			continue
 		}
-		if len(it.end) > 0 && bytes.Compare(it.end, it.Key()) < 0 {
+		if len(it.end) > 0 && bytes.Compare(it.Key(), it.end) > 0 {
 			break
 		}
 		return true
@@ -38,10 +44,14 @@ func (it *prefixIterator) Next() bool {
 	return false
 }
 
+// Key returns the key after removing the prefix. The caller can expect the length of the
+// returned byte slice to be [keyLen].
 func (it *prefixIterator) Key() []byte {
 	return it.Iterator.Key()[it.prefixLen:]
 }
 
+// valueWrapperIterator wraps an [ethdb.Iterator] and applies a given transformation function
+// to values before returning them.
 type valueWrapperIterator struct {
 	ethdb.Iterator
 	f   func([]byte) ([]byte, error)
@@ -56,10 +66,17 @@ func NewValueWrapperIterator(it ethdb.Iterator, f func([]byte) ([]byte, error)) 
 	}
 }
 
+// Value returns the result of the transformation function being applied to values of the
+// underlying iterator
 func (it *valueWrapperIterator) Value() []byte {
 	return it.val
 }
 
+// Next moves the iterator to the next value, and applies the tranformation function to the
+// underlying iterator's value, so it can be returned from [it.Value()].
+// If an error occurs in applying the transformation function, Next returns false and
+// the error can be retrieved by calling [it.Error()].
+// Returns true if there are more items to iterate.
 func (it *valueWrapperIterator) Next() bool {
 	if it.Iterator.Next() {
 		it.val, it.err = it.f(it.Iterator.Value())
@@ -68,6 +85,8 @@ func (it *valueWrapperIterator) Next() bool {
 	return false
 }
 
+// Error returns an error if one occurred while calling the transformation function or
+// if an error occurred in the underlying iterator.
 func (it *valueWrapperIterator) Error() error {
 	if it.err != nil {
 		return it.err
--- core/state/snapshot/iterator_disk.go
+++ core/state/snapshot/iterator_disk.go
@@ -9,15 +9,20 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// NewStorageSnapshotIterator returns an iterator over storage slots of [account].
+// Note: Removes storage prefixes and the account hash from keys before returning them.
 func NewStorageSnapshotIterator(db ethdb.Iteratee, account common.Hash, start, end []byte) ethdb.Iterator {
 	prefix := append([]byte(nil), rawdb.SnapshotStoragePrefix...)
 	prefix = append(prefix, account[:]...)
-	return rawdb.NewPrefixIterator(db, prefix, start, end)
+	return rawdb.NewPrefixIterator(db, prefix, common.HashLength, start, end)
 }
 
+// NewAccountSnapshotIterator returns an iterator over accounts in the snapshot.
+// Values are returned in consensus format (FullAccountRLP) for compatibility with
+// values stored in the trie.
 func NewAccountSnapshotIterator(db ethdb.Iteratee, start, end []byte) ethdb.Iterator {
 	return rawdb.NewValueWrapperIterator(
-		rawdb.NewPrefixIterator(db, rawdb.SnapshotAccountPrefix, start, end),
+		rawdb.NewPrefixIterator(db, rawdb.SnapshotAccountPrefix, common.HashLength, start, end),
 		FullAccountRLP,
 	)
 }
