e7cf3377a90b00b7c2d96c242c932a5690a53e91
abenegia <alberto.benegiamo@gmail.com>
2022-06-22 18:34:35 +0200

    fixed version in version.sh

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,4 +3,4 @@
 # Set up the versions to be used
 coreth_version=${CORETH_VERSION:-'v0.8.12'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.7.13-rc.4'}
+avalanche_version=${AVALANCHE_VERSION:-'pchain-tx-repack-step-4-point-5'}
