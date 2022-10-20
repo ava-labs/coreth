1ffca56ea2155461f880cee941c825984d9c0a95
Dan Laine <daniel.laine@avalabs.org>
2022-10-20 15:48:54 -0400

    update avalanche version in e2e test

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,4 +3,4 @@
 # Set up the versions to be used
 coreth_version=${CORETH_VERSION:-'v0.11.0'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.9.1-rc.3'}
+avalanche_version=${AVALANCHE_VERSION:-'92bbb85eb49a8226e11ce9ea799c0dd47cc99791'}
