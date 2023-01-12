5921c75c12bfff121f0cf11d02f248159630b6f8
peak3d <pfau@peak3d.de>
2023-01-12 11:53:12 +0100

    [Dependencies] Update caminogo (Tiered genesis allocations) / Check for existing linter

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,4 +7,7 @@ set -o pipefail
 # Initialize dependencies
 git submodule update --init
 
+if ! [ -x "$(command -v golangci-lint)" ]; then
+  go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@v1.49.0
+fi
 golangci-lint run --path-prefix=. --timeout 3m
