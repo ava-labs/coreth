1109849fc0161f68824fd625812e9f60ddda8c9b
Delweng <delweng@gmail.com>
2023-05-25 20:25:58 +0800

    accounts: replace noarg fmt.Errorf with errors.New (#27331)
    * accounts: replace noarg fmt.Errorf with errors.New
    
    Signed-off-by: jsvisa <delweng@gmail.com>
    
    * accounts: go autoimport
    
    Signed-off-by: jsvisa <delweng@gmail.com>
    
    ---------
    
    Signed-off-by: jsvisa <delweng@gmail.com>
    Signed-off-by: Darioush Jalali <darioush.jalali@avalabs.org>

--- accounts/keystore/account_cache_test.go
+++ accounts/keystore/account_cache_test.go
@@ -27,6 +27,7 @@
 package keystore
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"os"
@@ -84,7 +85,7 @@ func waitForAccounts(wantAccounts []accounts.Account, ks *KeyStore) error {
 			select {
 			case <-ks.changes:
 			default:
-				return fmt.Errorf("wasn't notified of new accounts")
+				return errors.New("wasn't notified of new accounts")
 			}
 			return nil
 		}
