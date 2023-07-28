fe5fd8ee2208bf8375cbcfa834a71335ed522718
Mario Vega <marioevz@gmail.com>
2023-07-28 01:08:38 -0600

    core/types: fix receipt blob fields marshaling (#27793)
    Signed-off-by: Darioush Jalali <darioush.jalali@avalabs.org>

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -90,6 +90,8 @@ type receiptMarshaling struct {
 	CumulativeGasUsed hexutil.Uint64
 	GasUsed           hexutil.Uint64
 	EffectiveGasPrice *hexutil.Big
+	BlobGasUsed       hexutil.Uint64
+	BlobGasPrice      *hexutil.Big
 	BlockNumber       *hexutil.Big
 	TransactionIndex  hexutil.Uint
 }
