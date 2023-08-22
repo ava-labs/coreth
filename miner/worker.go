cc8058e7b092e2fa280ddb14acd9786e1466e369
shuo <shuoli84@gmail.com>
2023-08-22 15:59:38 +0800

    miner: fix reply -> replay typo (#27961)
    * typo: reply -> replay
    
    * rebuild
    
    Signed-off-by: Darioush Jalali <darioush.jalali@avalabs.org>

--- miner/worker.go
+++ miner/worker.go
@@ -253,7 +253,7 @@ func (w *worker) commitTransactions(env *environment, txs *types.TransactionsByP
 		// Check whether the tx is replay protected. If we're not in the EIP155 hf
 		// phase, start ignoring the sender until we do.
 		if tx.Protected() && !w.chainConfig.IsEIP155(env.header.Number) {
-			log.Trace("Ignoring reply protected transaction", "hash", tx.Hash(), "eip155", w.chainConfig.EIP155Block)
+			log.Trace("Ignoring replay protected transaction", "hash", tx.Hash(), "eip155", w.chainConfig.EIP155Block)
 			txs.Pop()
 			continue
 		}
