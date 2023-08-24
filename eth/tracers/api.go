94ac1c36c614af332ff360d0d1fdf4e2a1e12f47
Felix Lange <fjl@twurst.com>
2023-08-24 10:47:42 +0200

    all: use rlp.DecodeBytes instead of rlp.Decode where possible (#27994)
    Signed-off-by: Darioush Jalali <darioush.jalali@avalabs.org>

--- eth/tracers/api.go
+++ eth/tracers/api.go
@@ -28,7 +28,6 @@ package tracers
 
 import (
 	"bufio"
-	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -484,7 +483,7 @@ func (api *API) TraceBlockByHash(ctx context.Context, hash common.Hash, config *
 // and returns them as a JSON object.
 func (api *baseAPI) TraceBlock(ctx context.Context, blob hexutil.Bytes, config *TraceConfig) ([]*txTraceResult, error) {
 	block := new(types.Block)
-	if err := rlp.Decode(bytes.NewReader(blob), block); err != nil {
+	if err := rlp.DecodeBytes(blob, block); err != nil {
 		return nil, fmt.Errorf("could not decode block: %v", err)
 	}
 	return api.traceBlock(ctx, block, config)
