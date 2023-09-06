luxfi/sdk

all: rename dataGas to blobGas (#27789)

core/types/gen_header_rlp.go
@@ -42,8 +42,8 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 	w.WriteBytes(obj.Nonce[:])
 	_tmp1 := obj.BaseFee != nil
 	_tmp2 := obj.WithdrawalsHash != nil
-	_tmp3 := obj.ExcessDataGas != nil
-	_tmp4 := obj.DataGasUsed != nil
+	_tmp3 := obj.ExcessBlobGas != nil
+	_tmp4 := obj.BlobGasUsed != nil
 	if _tmp1 || _tmp2 || _tmp3 || _tmp4 {
 		if obj.BaseFee == nil {
 			w.Write(rlp.EmptyString)
@@ -62,17 +62,17 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 		}
 	}
 	if _tmp3 || _tmp4 {
-		if obj.ExcessDataGas == nil {
+		if obj.ExcessBlobGas == nil {
 			w.Write([]byte{0x80})
 		} else {
-			w.WriteUint64((*obj.ExcessDataGas))
+			w.WriteUint64((*obj.ExcessBlobGas))
 		}
 	}
 	if _tmp4 {
-		if obj.DataGasUsed == nil {
+		if obj.BlobGasUsed == nil {
 			w.Write([]byte{0x80})
 		} else {
-			w.WriteUint64((*obj.DataGasUsed))
+			w.WriteUint64((*obj.BlobGasUsed))
 		}
 	}
 	w.ListEnd(_tmp0)

ava-labs/coreth

Signed-off-by: Darioush Jalali <darioush.jalali@avalabs.org>

core/types/gen_header_rlp.go
@@ -44,8 +44,8 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 	_tmp1 := obj.BaseFee != nil
 	_tmp2 := obj.ExtDataGasUsed != nil
 	_tmp3 := obj.BlockGasCost != nil
-	_tmp4 := obj.ExcessDataGas != nil
-	_tmp5 := obj.DataGasUsed != nil
+	_tmp4 := obj.ExcessBlobGas != nil
+	_tmp5 := obj.BlobGasUsed != nil
 	if _tmp1 || _tmp2 || _tmp3 || _tmp4 || _tmp5 {
 		if obj.BaseFee == nil {
 			w.Write(rlp.EmptyString)
@@ -77,17 +77,17 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 		}
 	}
 	if _tmp4 || _tmp5 {
-		if obj.ExcessDataGas == nil {
+		if obj.ExcessBlobGas == nil {
 			w.Write([]byte{0x80})
 		} else {
-			w.WriteUint64((*obj.ExcessDataGas))
+			w.WriteUint64((*obj.ExcessBlobGas))
 		}
 	}
 	if _tmp5 {
-		if obj.DataGasUsed == nil {
+		if obj.BlobGasUsed == nil {
 			w.Write([]byte{0x80})
 		} else {
-			w.WriteUint64((*obj.DataGasUsed))
+			w.WriteUint64((*obj.BlobGasUsed))
 		}
 	}
 	w.ListEnd(_tmp0)