fc088a944e7601495bdc97a2afee4bf7c448a135
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2023-12-06 15:28:46 -0500

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'app-error-0.12.9'}
+avalanche_version=${AVALANCHE_VERSION:-'app-error-0.12.9-rc.1'}
