633365ede18d0598ce928f99c2b7302c77f907b1
Darioush Jalali <darioush.jalali@avalabs.org>
2023-12-27 20:19:51 -0800

    precompile/contract: remove unneeded method from StateDB interface

--- core/vm/interface.go
+++ core/vm/interface.go
@@ -58,7 +58,6 @@ type StateDB interface {
 
 	Suicide(common.Address) bool
 	HasSuicided(common.Address) bool
-	Finalise(deleteEmptyObjects bool)
 
 	// Exist reports whether the given account exists in state.
 	// Notably this should also return true for suicided accounts.
