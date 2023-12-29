b3947cec9b13b7fd03b4322769d8fbe36db58c02
Fuyao Zhao <zfy0701@gmail.com>
2023-12-29 13:50:24 +0800

    support multiple txs in tracecallmany

--- eth/tracers/api.go
+++ eth/tracers/api.go
@@ -1178,6 +1178,7 @@ func (api *API) traceBundle(ctx context.Context, bundle *Bundle, simulateContext
 	if config != nil && config.Reexec != nil {
 		reexec = *config.Reexec
 	}
+	is158 := api.backend.ChainConfig().IsEIP158(block.Number())
 
 	if err != nil {
 		return nil, err
@@ -1197,10 +1198,6 @@ func (api *API) traceBundle(ctx context.Context, bundle *Bundle, simulateContext
 	}
 	// Execute the trace
 	for idx, args := range bundle.Transactions {
-		if idx > 0 {
-			// TODO currently only support one tx in bundle
-			break
-		}
 		if args.Gas == nil {
 			gasCap := api.backend.RPCGasCap()
 			args.Gas = (*hexutil.Uint64)(&gasCap)
@@ -1214,11 +1211,17 @@ func (api *API) traceBundle(ctx context.Context, bundle *Bundle, simulateContext
 		if config != nil {
 			traceConfig = &config.TraceConfig
 		}
-		r, err := api.traceTx(ctx, msg, new(Context), vmctx, statedb, traceConfig)
+		txctx := &Context{
+			BlockHash:   block.Hash(),
+			BlockNumber: block.Number(),
+			TxIndex:     simulateContext.TransactionIndex + idx,
+		}
+		r, err := api.traceTx(ctx, msg, txctx, vmctx, statedb, traceConfig)
 		if err != nil {
 			return nil, err
 		}
 		result = append(result, r)
+		statedb.Finalise(is158)
 	}
 	return result, nil
 }
