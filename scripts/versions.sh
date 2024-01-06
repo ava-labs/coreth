9cfb9186c70c948ace79d29c85195dde8540c71b
Patrick O'Grady <prohb125@gmail.com>
2024-01-05 16:18:56 -0800

    fix e2e

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.10.18-rc.15'}
+avalanche_version=${AVALANCHE_VERSION:-'790b8a522a5c9068631272ef2df0e3a6e383db6d'}
