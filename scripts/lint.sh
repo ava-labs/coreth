cdb06b4ac052616060b551c34dd9a2f4b45f965b
Maru Newby <maru.newby@avalabs.org>
2024-01-08 16:57:22 +0100

    fixup: Avoid requiring patch version

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,12 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-KNOWN_GOOD_VERSION="v1.54.2"
 # Upstream is compatible with v1.54.x at time of this writing, and
 # checking for this specific version is an attempt to avoid skew
-# between local and CI execution. The latest version (v1.55.1) seems to
-# cause spurious failures
-VERSION="$(golangci-lint --version | sed -e 's+golangci-lint has version \(v1.*\) built.*+\1+')"
+# between local and CI execution. The latest version (v1.55.1) seems
+# to cause spurious failures
+KNOWN_GOOD_VERSION="v1.54"
+VERSION="$(golangci-lint --version | sed -e 's+golangci-lint has version \(v1.*\)\..* built.*+\1+')"
 if [[ "${VERSION}" != "${KNOWN_GOOD_VERSION}" ]]; then
   echo "expected golangci-lint ${KNOWN_GOOD_VERSION}, but ${VERSION} was used"
   echo "${KNOWN_GOOD_VERSION} is used in CI and should be used locally to ensure compatible results"
