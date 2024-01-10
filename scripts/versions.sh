46008cac91c79dce1aa63fbc440a9695a8d874ea
Patrick O'Grady <prohb125@gmail.com>
2024-01-10 08:10:44 +0100

    use correct avalanche version

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'790b8a522a5c9068631272ef2df0e3a6e383db6d'}
+avalanche_version=${AVALANCHE_VERSION:-'a905aaea3b060d719951920b4d2d5cce38fcd5d2'}
