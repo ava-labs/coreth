cbd90df94d6dcb908fdfcf65984dba5ba15fddc7
Patrick O'Grady <prohb125@gmail.com>
2024-01-10 10:50:20 +0100

    update scripts version

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'a905aaea3b060d719951920b4d2d5cce38fcd5d2'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.10.18-rc.17'}
