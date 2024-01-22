99be2c426b30d561d35963f544efe1ef9f8863ee
Ceyhun Onur <ceyhun.onur@avalabs.org>
2024-01-22 18:45:29 +0300

    use latest avalanchego

--- scripts/tests.e2e.sh
+++ scripts/tests.e2e.sh
@@ -13,10 +13,7 @@ if ! [[ "$0" =~ scripts/tests.e2e.sh ]]; then
 fi
 
 # Coreth root directory
-CORETH_PATH=$(
-  cd "$(dirname "${BASH_SOURCE[0]}")"
-  cd .. && pwd
-)
+CORETH_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
 
 # Allow configuring the clone path to point to an existing clone
 AVALANCHEGO_CLONE_PATH="${AVALANCHEGO_CLONE_PATH:-avalanchego}"
@@ -33,25 +30,15 @@ trap cleanup EXIT
 echo "checking out target AvalancheGo version ${avalanche_version}"
 if [[ -d "${AVALANCHEGO_CLONE_PATH}" ]]; then
   echo "updating existing clone"
+  cd "${AVALANCHEGO_CLONE_PATH}"
+  git fetch
 else
   echo "creating new clone"
   git clone https://github.com/ava-labs/avalanchego.git "${AVALANCHEGO_CLONE_PATH}"
+  cd "${AVALANCHEGO_CLONE_PATH}"
 fi
-
-cd "${AVALANCHEGO_CLONE_PATH}"
-git fetch origin ${avalanche_version}
-
-ref=""
-# check if given reference is a tag or branch
-if git rev-parse --quiet --verify "origin/${avalanche_version}^{commit}" >/dev/null; then
-  echo "checking out branch ${avalanche_version}"
-  ref="origin/${avalanche_version}"
-else
-  echo "checking out tag ${avalanche_version}"
-  ref="${avalanche_version}"
-fi
-
-git checkout -B "test-${avalanche_version}" "${ref}"
+# Branch will be reset to $avalanche_version if it already exists
+git checkout -B "test-${avalanche_version}" "${avalanche_version}"
 
 echo "updating coreth dependency to point to ${CORETH_PATH}"
 go mod edit -replace "github.com/ava-labs/coreth=${CORETH_PATH}"
