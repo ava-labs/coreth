c0d271b9e813639501feba7248750644b4b6f6a6
Patrick O'Grady <prohb125@gmail.com>
2024-02-27 12:38:57 -0800

    re-add legacy handling tests

---
+++ plugin/evm/gossiper_eth_gossiping_test.go
@@ -0,0 +1,107 @@
+// (c) 2019-2021, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package evm
+
+import (
+	"context"
+	"encoding/json"
+	"math/big"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/set"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ava-labs/coreth/core"
+	"github.com/ava-labs/coreth/core/types"
+	"github.com/ava-labs/coreth/params"
+	"github.com/ava-labs/coreth/plugin/evm/message"
+)
+
+func fundAddressByGenesis(addrs []common.Address) (string, error) {
+	balance := big.NewInt(0xffffffffffffff)
+	genesis := &core.Genesis{
+		Difficulty: common.Big0,
+		GasLimit:   uint64(5000000),
+	}
+	funds := make(map[common.Address]core.GenesisAccount)
+	for _, addr := range addrs {
+		funds[addr] = core.GenesisAccount{
+			Balance: balance,
+		}
+	}
+	genesis.Alloc = funds
+	genesis.Config = params.TestChainConfig
+
+	bytes, err := json.Marshal(genesis)
+	return string(bytes), err
+}
+
+// show that a geth tx discovered from gossip is requested to the same node that
+// gossiped it
+func TestMempoolEthTxsAppGossipHandling(t *testing.T) {
+	if os.Getenv("RUN_FLAKY_TESTS") != "true" {
+		t.Skip("FLAKY")
+	}
+	assert := assert.New(t)
+
+	key, err := crypto.GenerateKey()
+	assert.NoError(err)
+
+	addr := crypto.PubkeyToAddress(key.PublicKey)
+
+	genesisJSON, err := fundAddressByGenesis([]common.Address{addr})
+	assert.NoError(err)
+
+	_, vm, _, _, sender := GenesisVM(t, true, genesisJSON, "", "")
+	defer func() {
+		err := vm.Shutdown(context.Background())
+		assert.NoError(err)
+	}()
+	vm.txPool.SetGasPrice(common.Big1)
+	vm.txPool.SetMinFee(common.Big0)
+
+	var (
+		wg          sync.WaitGroup
+		txRequested bool
+	)
+	sender.CantSendAppGossip = false
+	sender.SendAppRequestF = func(context.Context, set.Set[ids.NodeID], uint32, []byte) error {
+		txRequested = true
+		return nil
+	}
+	wg.Add(1)
+	sender.SendAppGossipF = func(context.Context, []byte) error {
+		wg.Done()
+		return nil
+	}
+
+	// prepare a tx
+	tx := getValidEthTxs(key, 1, common.Big1)[0]
+
+	// show that unknown coreth hashes is requested
+	txBytes, err := rlp.EncodeToBytes([]*types.Transaction{tx})
+	assert.NoError(err)
+	msg := message.EthTxsGossip{
+		Txs: txBytes,
+	}
+	msgBytes, err := message.BuildGossipMessage(vm.networkCodec, msg)
+	assert.NoError(err)
+
+	nodeID := ids.GenerateTestNodeID()
+	err = vm.AppGossip(context.Background(), nodeID, msgBytes)
+	assert.NoError(err)
+	assert.False(txRequested, "tx should not be requested")
+
+	// wait for transaction to be re-gossiped
+	attemptAwait(t, &wg, 5*time.Second)
+}
