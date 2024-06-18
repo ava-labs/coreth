8e570821202374320a080cf9c20b04c7df7a140f
Darioush Jalali <darioush.jalali@avalabs.org>
2024-06-17 17:19:12 -0700

    wip: blockchain getter scaffolding

--- plugin/evm/interfaces.go
+++ plugin/evm/interfaces.go
@@ -27,7 +27,6 @@ type BlockChain interface {
 	GetBlockByNumber(uint64) *types.Block
 	InitializeSnapshots()
 	HasBlock(common.Hash, uint64) bool
-	GetBlock(common.Hash, uint64) *types.Block
 	DrainAcceptorQueue()
 	HasState(common.Hash) bool
 	State() (StateDB, error)
