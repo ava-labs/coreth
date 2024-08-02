cbb1ecabbe26ec2e8cb4edf0f5d12b2c84d433c8
Darioush Jalali <darioush.jalali@avalabs.org>
2024-08-02 09:02:48 -0700

    nits, update comments

--- params/avalanche_params.go
+++ params/avalanche_params.go
@@ -21,12 +21,12 @@ const (
 	ApricotPhase1GasLimit uint64 = 8_000_000
 	CortinaGasLimit       uint64 = 15_000_000
 
-	ApricotPhase3MinBaseFee               int64  = 75_000_000_000
-	ApricotPhase3MaxBaseFee               int64  = 225_000_000_000
-	ApricotPhase3InitialBaseFee           int64  = 225_000_000_000
+	ApricotPhase3MinBaseFee               int64  = 75 * GWei
+	ApricotPhase3MaxBaseFee               int64  = 225 * GWei
+	ApricotPhase3InitialBaseFee           int64  = 225 * GWei
 	ApricotPhase3TargetGas                uint64 = 10_000_000
-	ApricotPhase4MinBaseFee               int64  = 25_000_000_000
-	ApricotPhase4MaxBaseFee               int64  = 1_000_000_000_000
+	ApricotPhase4MinBaseFee               int64  = 25 * GWei
+	ApricotPhase4MaxBaseFee               int64  = 1_000 * GWei
 	ApricotPhase4BaseFeeChangeDenominator uint64 = 12
 	ApricotPhase5TargetGas                uint64 = 15_000_000
 	ApricotPhase5BaseFeeChangeDenominator uint64 = 36
