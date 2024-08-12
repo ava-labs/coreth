d0b8f0c2fadf8b3b99baddbf948ba4748972eaa4
Darioush Jalali <darioush.jalali@avalabs.org>
2024-08-12 16:22:12 -0700

    rename to subnet-evm

--- miner/ordering_ext.go
+++ miner/ordering_ext.go
@@ -3,8 +3,8 @@ package miner
 import (
 	"math/big"
 
-	"github.com/ava-labs/coreth/core/txpool"
-	"github.com/ava-labs/coreth/core/types"
+	"github.com/ava-labs/subnet-evm/core/txpool"
+	"github.com/ava-labs/subnet-evm/core/types"
 	"github.com/ethereum/go-ethereum/common"
 )
 
--- plugin/evm/tx.go
+++ plugin/evm/tx.go
@@ -12,8 +12,8 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/ava-labs/coreth/core/state"
-	"github.com/ava-labs/coreth/params"
+	"github.com/ava-labs/subnet-evm/core/state"
+	"github.com/ava-labs/subnet-evm/params"
 
 	"github.com/ava-labs/avalanchego/chains/atomic"
 	"github.com/ava-labs/avalanchego/codec"
