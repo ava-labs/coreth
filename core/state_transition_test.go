30f8ee6b81334f1f25294e6abc24c2e9b28978fa
Darioush Jalali <darioush.jalali@avalabs.org>
2024-09-27 08:39:09 -0700

    set timestamps in chain config

--- core/state_transition_test.go
+++ core/state_transition_test.go
@@ -183,9 +183,6 @@ func TestNativeAssetContractCall(t *testing.T) {
 
 	for name, stTest := range tests {
 		t.Run(name, func(t *testing.T) {
-			cpy := *stTest.config
-			cpy.SetEthUpgrades()
-			stTest.config = &cpy
 			executeStateTransitionTest(t, stTest)
 		})
 	}
