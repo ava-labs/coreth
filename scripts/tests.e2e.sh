f8f597dea0c194de26dd231611a947eab76c6827
Darioush Jalali <darioush.jalali@avalabs.org>
2024-11-21 14:40:39 -0800

    fix

--- scripts/tests.e2e.sh
+++ scripts/tests.e2e.sh
@@ -45,7 +45,7 @@ git checkout -B "test-${AVALANCHE_VERSION}" "${AVALANCHE_VERSION}"
 
 echo "updating coreth dependency to point to ${CORETH_PATH}"
 go mod edit -replace "github.com/ava-labs/coreth=${CORETH_PATH}"
-go mod edit -replace "github.com/ava-labs/libevm@v0.0.0-20241121221822-8486d85dbf1f"
+go mod edit -replace "github.com/ava-labs/libevm=github.com/ava-labs/libevm@v0.0.0-20241121221822-8486d85dbf1f"
 go mod tidy
 
 echo "building avalanchego"
