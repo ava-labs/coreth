515ce06594354f143f39aba3da356b7c562fd870
Darioush Jalali <darioush.jalali@avalabs.org>
2024-12-18 21:43:31 -0800

    try

--- plugin/evm/reprocess_test.go
+++ plugin/evm/reprocess_test.go
@@ -201,7 +201,7 @@ func TestCalculatePrefix(t *testing.T) {
 
 func init() {
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGPIPE, syscall.SIGHUP)
 	go cleanupOnInterrupt(c)
 }
 
