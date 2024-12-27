60d702a568261f8ab97ad90496e63286d2eddd80
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2024-12-27 11:18:32 +0100

    Generate mock with license header

--- internal/ethapi/mocks_generate_test.go
+++ internal/ethapi/mocks_generate_test.go
@@ -1,3 +1,3 @@
 package ethapi
 
-//go:generate mockgen -package=$GOPACKAGE -destination=mocks_test.go . Backend
+//go:generate mockgen -package=$GOPACKAGE -destination=mocks_test.go -copyright_file=../../license_header . Backend
