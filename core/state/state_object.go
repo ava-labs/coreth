58a793717941d01f7b7908f33c6b402f0fac6bfa
Darioush Jalali <darioush.jalali@avalabs.org>
2025-01-10 15:14:12 -0800

    try

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -227,6 +227,10 @@ func (s *stateObject) GetCommittedState(key common.Hash) common.Hash {
 			s.db.setError(err)
 			return common.Hash{}
 		}
+		if crypto.Keccak256Hash(key.Bytes()) == common.Hash(target) {
+			fmt.Println("GetState: ", s.address, key, value)
+			panic("found")
+		}
 		val, err := tr.GetStorage(s.address, key.Bytes())
 		if metrics.EnabledExpensive {
 			s.db.StorageReads += time.Since(start)
@@ -240,6 +244,8 @@ func (s *stateObject) GetCommittedState(key common.Hash) common.Hash {
 	s.originStorage[key] = value
 	return value
 }
+
+var target = common.Hex2Bytes("171ab08901be24769dbebedbdf7e0245486fbc64ab975cd431a39533032d5415")
 
 // SetState updates a value in account storage.
 func (s *stateObject) SetState(key, value common.Hash) {
