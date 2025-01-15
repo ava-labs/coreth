70e37df31e8daa3fa59bf0e4f5b2d84ae2032ec6
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-01-15 10:37:46 +0100

    Forgotten comment revert

--- plugin/evm/config/config.go
+++ plugin/evm/config/config.go
@@ -26,7 +26,7 @@ const (
 	defaultSyncableCommitInterval                 = defaultCommitInterval * 4
 	defaultSnapshotWait                           = false
 	defaultRpcGasCap                              = 50_000_000 // Default to 50M Gas Limit
-	defaultRpcTxFeeCap                            = 100
+	defaultRpcTxFeeCap                            = 100        // 100 AVAX
 	defaultMetricsExpensiveEnabled                = true
 	defaultApiMaxDuration                         = 0 // Default to no maximum API call duration
 	defaultWsCpuRefillRate                        = 0 // Default to no maximum WS CPU usage
