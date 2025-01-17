bd8c78a400bc78455bf3a9c7eadad0e0774a40c4
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-01-17 15:19:41 +0100

    feat(core/types): `Body` and `Block` hooks

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -265,7 +265,7 @@ func (ec *client) getBlock(ctx context.Context, method string, args ...interface
 		}
 		txs[i] = tx.tx
 	}
-	return types.NewBlockWithHeader(head).WithBody(txs, uncles).WithExtData(body.Version, (*[]byte)(body.BlockExtraData)), nil
+	return types.NewBlockWithHeader(head).WithBody(types.Body{Transactions: txs, Uncles: uncles}), nil
 }
 
 // HeaderByHash returns the block header with the given hash.
