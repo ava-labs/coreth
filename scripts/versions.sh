425b4f207796ffecd84edcb18d51ba767b7a6960
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2025-01-29 10:59:31 -0500

    update hash.

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -6,4 +6,4 @@
 set -euo pipefail
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'cddfa5d0'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'2eea9338'}
