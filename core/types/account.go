d71d738a11b64aa84c321c3238d8d677bb77dcf9
Darioush Jalali <darioush.jalali@avalabs.org>
2025-02-04 17:15:01 -0800

    chore: fix lint

--- core/types/account.go
+++ core/types/account.go
@@ -17,50 +17,13 @@
 package types
 
 import (
-	"bytes"
-	"encoding/hex"
-	"fmt"
-
-	"github.com/ava-labs/libevm/common"
-	"github.com/ava-labs/libevm/common/hexutil"
-	"github.com/ava-labs/libevm/common/math"
 	ethtypes "github.com/ava-labs/libevm/core/types"
 )
-
-//go:generate go run github.com/fjl/gencodec -type Account -field-override accountMarshaling -out gen_account.go
 
 // Account represents an Ethereum account and its attached data.
 // This type is used to specify accounts in the genesis block state, and
 // is also useful for JSON encoding/decoding of accounts.
 type Account = ethtypes.Account
-
-type accountMarshaling struct {
-	Code       hexutil.Bytes
-	Balance    *math.HexOrDecimal256
-	Nonce      math.HexOrDecimal64
-	Storage    map[storageJSON]storageJSON
-	PrivateKey hexutil.Bytes
-}
-
-// storageJSON represents a 256 bit byte array, but allows less than 256 bits when
-// unmarshaling from hex.
-type storageJSON common.Hash
-
-func (h *storageJSON) UnmarshalText(text []byte) error {
-	text = bytes.TrimPrefix(text, []byte("0x"))
-	if len(text) > 64 {
-		return fmt.Errorf("too many hex characters in storage key/value %q", text)
-	}
-	offset := len(h) - len(text)/2 // pad on the left
-	if _, err := hex.Decode(h[offset:], text); err != nil {
-		return fmt.Errorf("invalid hex storage key/value %q", text)
-	}
-	return nil
-}
-
-func (h storageJSON) MarshalText() ([]byte, error) {
-	return hexutil.Bytes(h[:]).MarshalText()
-}
 
 // GenesisAlloc specifies the initial state of a genesis block.
 type GenesisAlloc = ethtypes.GenesisAlloc
