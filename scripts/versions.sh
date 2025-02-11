3eb68c6132f24d7056a2c8d9c789d6d9f169164f
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-02-11 21:04:47 +0300

    bump e2e version

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -6,4 +6,4 @@
 set -euo pipefail
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'286645b'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'c2c6e5dd'}
