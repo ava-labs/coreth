aa49251e995921f1546e9c00a388ff4e0d10511d
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-02-12 15:41:14 +0100

    Add `TestHeaderSerializable_updates`

--- core/types/header_ext_test.go
+++ core/types/header_ext_test.go
@@ -4,8 +4,11 @@
 package types
 
 import (
+	"math/big"
 	"testing"
 
+	"github.com/ava-labs/libevm/common"
+	ethtypes "github.com/ava-labs/libevm/core/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -28,4 +31,143 @@ func TestHeaderExtraGetWith(t *testing.T) {
 	assert.Equal(t, &HeaderExtra{
 		ExtDataHash: [32]byte{1},
 	}, extra)
+}
+
+func TestHeaderSerializable_updates(t *testing.T) {
+	t.Parallel()
+
+	t.Run("from", func(t *testing.T) {
+		t.Parallel()
+
+		eth := &ethtypes.Header{
+			ParentHash:       common.Hash{1},
+			UncleHash:        common.Hash{2},
+			Coinbase:         common.Address{3},
+			Root:             common.Hash{4},
+			TxHash:           common.Hash{5},
+			ReceiptHash:      common.Hash{6},
+			Bloom:            Bloom{7},
+			Difficulty:       big.NewInt(8),
+			Number:           big.NewInt(9),
+			GasLimit:         10,
+			GasUsed:          11,
+			Time:             12,
+			Extra:            []byte{13},
+			MixDigest:        common.Hash{14},
+			Nonce:            BlockNonce{15},
+			BaseFee:          big.NewInt(16),
+			WithdrawalsHash:  &common.Hash{17},
+			BlobGasUsed:      ptrTo(uint64(18)),
+			ExcessBlobGas:    ptrTo(uint64(19)),
+			ParentBeaconRoot: &common.Hash{20},
+		}
+		extras := &HeaderExtra{
+			ExtDataHash:    common.Hash{21},
+			ExtDataGasUsed: big.NewInt(22),
+			BlockGasCost:   big.NewInt(23),
+		}
+
+		got := new(HeaderSerializable)
+		got.updateFromEth(eth)
+		got.updateFromExtras(extras)
+
+		allFieldsAreSet(t, got)
+
+		want := &HeaderSerializable{
+			ParentHash:       common.Hash{1},
+			UncleHash:        common.Hash{2},
+			Coinbase:         common.Address{3},
+			Root:             common.Hash{4},
+			TxHash:           common.Hash{5},
+			ReceiptHash:      common.Hash{6},
+			Bloom:            Bloom{7},
+			Difficulty:       big.NewInt(8),
+			Number:           big.NewInt(9),
+			GasLimit:         10,
+			GasUsed:          11,
+			Time:             12,
+			Extra:            []byte{13},
+			MixDigest:        common.Hash{14},
+			Nonce:            BlockNonce{15},
+			BaseFee:          big.NewInt(16),
+			BlobGasUsed:      ptrTo(uint64(18)),
+			ExcessBlobGas:    ptrTo(uint64(19)),
+			ParentBeaconRoot: &common.Hash{20},
+			ExtDataHash:      common.Hash{21},
+			ExtDataGasUsed:   big.NewInt(22),
+			BlockGasCost:     big.NewInt(23),
+		}
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("to", func(t *testing.T) {
+		t.Parallel()
+
+		serializable := &HeaderSerializable{
+			ParentHash:       common.Hash{1},
+			UncleHash:        common.Hash{2},
+			Coinbase:         common.Address{3},
+			Root:             common.Hash{4},
+			TxHash:           common.Hash{5},
+			ReceiptHash:      common.Hash{6},
+			Bloom:            Bloom{7},
+			Difficulty:       big.NewInt(8),
+			Number:           big.NewInt(9),
+			GasLimit:         10,
+			GasUsed:          11,
+			Time:             12,
+			Extra:            []byte{13},
+			MixDigest:        common.Hash{14},
+			Nonce:            BlockNonce{15},
+			BaseFee:          big.NewInt(16),
+			BlobGasUsed:      ptrTo(uint64(18)),
+			ExcessBlobGas:    ptrTo(uint64(19)),
+			ParentBeaconRoot: &common.Hash{20},
+			ExtDataHash:      common.Hash{21},
+			ExtDataGasUsed:   big.NewInt(22),
+			BlockGasCost:     big.NewInt(23),
+		}
+		allFieldsAreSet(t, serializable)
+
+		eth := new(ethtypes.Header)
+		extras := new(HeaderExtra)
+
+		serializable.updateToEth(eth)
+		serializable.updateToExtras(extras)
+
+		// Note eth isn't checked for all its fields to be set
+		// since some of its fields may not be required to be set,
+		// such as WithdrawalsHash.
+		allFieldsAreSet(t, extras)
+
+		wantedEth := &ethtypes.Header{
+			ParentHash:       common.Hash{1},
+			UncleHash:        common.Hash{2},
+			Coinbase:         common.Address{3},
+			Root:             common.Hash{4},
+			TxHash:           common.Hash{5},
+			ReceiptHash:      common.Hash{6},
+			Bloom:            Bloom{7},
+			Difficulty:       big.NewInt(8),
+			Number:           big.NewInt(9),
+			GasLimit:         10,
+			GasUsed:          11,
+			Time:             12,
+			Extra:            []byte{13},
+			MixDigest:        common.Hash{14},
+			Nonce:            BlockNonce{15},
+			BaseFee:          big.NewInt(16),
+			BlobGasUsed:      ptrTo(uint64(18)),
+			ExcessBlobGas:    ptrTo(uint64(19)),
+			ParentBeaconRoot: &common.Hash{20},
+		}
+		wantedExtras := &HeaderExtra{
+			ExtDataHash:    common.Hash{21},
+			ExtDataGasUsed: big.NewInt(22),
+			BlockGasCost:   big.NewInt(23),
+		}
+
+		assert.Equal(t, wantedEth, eth)
+		assert.Equal(t, wantedExtras, extras)
+	})
 }
