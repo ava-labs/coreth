17cf418eb9e9a2559067cd9f3f90c7878746c10a
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-02-13 12:58:22 +0100

    Revert to compute test values without a generator

--- plugin/evm/message/code_request_test.go
+++ plugin/evm/message/code_request_test.go
@@ -33,13 +33,16 @@ func TestMarshalCodeRequest(t *testing.T) {
 // TestMarshalCodeResponse asserts that the structure or serialization logic hasn't changed, primarily to
 // ensure compatibility with the network.
 func TestMarshalCodeResponse(t *testing.T) {
-	codeData := []byte{0x52, 0xfd, 0xfc, 0x7, 0x21, 0x82, 0x65, 0x4f, 0x16, 0x3f, 0x5f, 0xf, 0x9a, 0x62, 0x1d, 0x72, 0x95, 0x66, 0xc7, 0x4d, 0x10, 0x3, 0x7c, 0x4d, 0x7b, 0xbb, 0x4, 0x7, 0xd1, 0xe2, 0xc6, 0x49, 0x81, 0x85, 0x5a, 0xd8, 0x68, 0x1d, 0xd, 0x86, 0xd1, 0xe9, 0x1e, 0x0, 0x16, 0x79, 0x39, 0xcb, 0x66, 0x94}
+	codeData := make([]byte, 50)
+	for i := range codeData {
+		codeData[i] = byte(i)
+	}
 
 	codeResponse := CodeResponse{
 		Data: [][]byte{codeData},
 	}
 
-	base64CodeResponse := "AAAAAAABAAAAMlL9/AchgmVPFj9fD5piHXKVZsdNEAN8TXu7BAfR4sZJgYVa2GgdDYbR6R4AFnk5y2aU"
+	const base64CodeResponse = "AAAAAAABAAAAMgABAgMEBQYHCAkKCwwNDg8QERITFBUWFxgZGhscHR4fICEiIyQlJicoKSorLC0uLzAx"
 
 	codeResponseBytes, err := Codec.Marshal(Version, codeResponse)
 	assert.NoError(t, err)
