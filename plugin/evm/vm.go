4a7edc6d9ebb49ab73a68ee5b1d2c36bc1e18e38
Stephen Buttolph <stephen@avalabs.org>
2025-02-14 17:27:00 -0500

    nit

--- plugin/evm/vm.go
+++ plugin/evm/vm.go
@@ -1585,16 +1585,12 @@ func (vm *VM) verifyTxAtTip(tx *atomic.Tx) error {
 		return fmt.Errorf("failed to retrieve block state at tip while verifying atomic tx: %w", err)
 	}
 	rules := vm.currentRules()
-
 	parentHeader := preferredBlock
 	timestamp := uint64(vm.clock.Time().Unix())
-	var nextBaseFee *big.Int
-	if vm.chainConfig.IsApricotPhase3(timestamp) {
-		nextBaseFee, err = header.EstimateNextBaseFee(vm.chainConfig, parentHeader, timestamp)
-		if err != nil {
-			// Return extremely detailed error since CalcBaseFee should never encounter an issue here
-			return fmt.Errorf("failed to calculate base fee with parent timestamp (%d), parent ExtraData: (0x%x), and current timestamp (%d): %w", parentHeader.Time, parentHeader.Extra, timestamp, err)
-		}
+	nextBaseFee, err := header.EstimateNextBaseFee(vm.chainConfig, parentHeader, timestamp)
+	if err != nil {
+		// Return extremely detailed error since CalcBaseFee should never encounter an issue here
+		return fmt.Errorf("failed to calculate base fee with parent timestamp (%d), parent ExtraData: (0x%x), and current timestamp (%d): %w", parentHeader.Time, parentHeader.Extra, timestamp, err)
 	}
 
 	// We don’t need to revert the state here in case verifyTx errors, because
