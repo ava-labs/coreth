fd89ac266e96a2c459c889372da0620f5c7d77ed
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-02-14 15:32:28 +0100

    Move clearPrefix to accessors_state_sync.go where it's used

--- core/rawdb/database_ext.go
+++ core/rawdb/database_ext.go
@@ -9,35 +9,6 @@ import (
 	"github.com/ava-labs/libevm/ethdb"
 )
 
-// clearPrefix removes all keys in db that begin with prefix and match an
-// expected key length. [keyLen] should include the length of the prefix.
-func clearPrefix(db ethdb.KeyValueStore, prefix []byte, keyLen int) error {
-	it := db.NewIterator(prefix, nil)
-	defer it.Release()
-
-	batch := db.NewBatch()
-	for it.Next() {
-		key := common.CopyBytes(it.Key())
-		if len(key) != keyLen {
-			// avoid deleting keys that do not match the expected length
-			continue
-		}
-		if err := batch.Delete(key); err != nil {
-			return err
-		}
-		if batch.ValueSize() > ethdb.IdealBatchSize {
-			if err := batch.Write(); err != nil {
-				return err
-			}
-			batch.Reset()
-		}
-	}
-	if err := it.Error(); err != nil {
-		return err
-	}
-	return batch.Write()
-}
-
 // InspectDatabase traverses the entire database and checks the size
 // of all different categories of data.
 func InspectDatabase(db ethdb.Database, keyPrefix, keyStart []byte) error {
