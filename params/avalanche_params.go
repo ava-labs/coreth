5703f5b13e08935b96207fd1b2094108f070479c
Stephen Buttolph <stephen@avalabs.org>
2025-02-18 14:49:15 -0500

    Move AP5 params

--- params/avalanche_params.go
+++ params/avalanche_params.go
@@ -4,8 +4,6 @@
 package params
 
 import (
-	"math/big"
-
 	"github.com/ava-labs/avalanchego/utils/units"
 	"github.com/ava-labs/avalanchego/utils/wrappers"
 )
@@ -32,12 +30,3 @@ const (
 	RollupWindow            = 10 // in seconds
 	DynamicFeeExtraDataSize = wrappers.LongLen * RollupWindow
 )
-
-// The atomic gas limit specifies the maximum amount of gas that can be consumed by the atomic
-// transactions included in a block and is enforced as of ApricotPhase5. Prior to ApricotPhase5,
-// a block included a single atomic transaction. As of ApricotPhase5, each block can include a set
-// of atomic transactions where the cumulative atomic gas consumed is capped by the atomic gas limit,
-// similar to the block gas limit.
-//
-// This value must always remain <= MaxUint64.
-var AtomicGasLimit *big.Int = big.NewInt(100_000)
