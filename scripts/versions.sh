60abba707c1722d5b035591432ba2982c70f988c
Stephen Buttolph <stephen@avalabs.org>
2025-02-25 10:33:05 -0500

    debugging...

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -6,4 +6,4 @@
 set -euo pipefail
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'be165bd5'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'37e79d4b'}
