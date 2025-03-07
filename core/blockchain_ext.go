2be09575d5c0d5992c003142444e5714cced32bc
Arran Schlosberg <me@arranschlosberg.com>
2025-03-07 11:22:33 +0000

    chore: correct copyright  header on new file

--- core/blockchain_ext.go
+++ core/blockchain_ext.go
@@ -1,4 +1,5 @@
-// (c) 2025, Ava Labs, Inc.
+// (c) 2024 Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
 package core
 
 import "github.com/ava-labs/libevm/metrics"
