c970f31501c754eadba811858391c5ad5f7d5d48
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-03-24 11:13:48 +0100

    utils/key.go: add comments to `Key` and `NewKey`

--- utils/key.go
+++ utils/key.go
@@ -11,11 +11,14 @@ import (
 	"github.com/ava-labs/libevm/crypto"
 )
 
+// Key contains an ecdsa private key field as well as an address field
+// obtained from converting the ecdsa public key.
 type Key struct {
 	Address    common.Address
 	PrivateKey *ecdsa.PrivateKey
 }
 
+// NewKey generates a new key pair and returns a pointer to a [Key].
 func NewKey() (*Key, error) {
 	privateKeyECDSA, err := ecdsa.GenerateKey(crypto.S256(), rand.Reader)
 	if err != nil {
