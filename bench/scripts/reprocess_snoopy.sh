2ffda404d18918201f8b23f270a91d3f6e45abaa
Aaron Buchwald <aaron.buchwald56@gmail.com>
2025-04-21 10:47:21 -0400

    add bash -x to reprocess snoopy

--- bench/scripts/reprocess_snoopy.sh
+++ bench/scripts/reprocess_snoopy.sh
@@ -8,4 +8,4 @@ set -o pipefail
 
 firewood_target_file="/home/snoopy/fw-test-$(date -Iseconds).db.eth.33m"
 cp /home/snoopy/fw.db.eth.33m.bak ${firewood_target_file}
-./bench/scripts/reprocess_blocks.sh /home/snoopy/blocks-mainnet-50m /home/snoopy/fw-metadata.33m ${firewood_target_file} 33133104 34000000
+bash -x ./bench/scripts/reprocess_blocks.sh /home/snoopy/blocks-mainnet-50m /home/snoopy/fw-metadata.33m ${firewood_target_file} 33133104 34000000
