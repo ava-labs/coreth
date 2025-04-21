9c81ee832b7f21366f52f9f050015d9c41b8e265
Aaron Buchwald <aaron.buchwald56@gmail.com>
2025-04-21 10:44:34 -0400

    update bench scripts

--- bench/scripts/import_s3_blocks.sh
+++ bench/scripts/import_s3_blocks.sh
@@ -7,8 +7,6 @@ set -o pipefail
 # Assumes golang 1.19+ and AWS credentials available in environment variables
 # Cloning this with s5cmd takes ~10 minutes
 
-go install github.com/peak/s5cmd/v2@master
-
 git clone https://github.com/peak/s5cmd.git
 cd s5cmd
 CGO_ENABLED=0 make build
