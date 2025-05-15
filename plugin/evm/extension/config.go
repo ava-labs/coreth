33007d2f460d593546de26ba7c4fbd6816fa42df
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-05-15 19:56:56 +0300

    commit unsaved file

--- plugin/evm/extension/config.go
+++ plugin/evm/extension/config.go
@@ -4,28 +4,10 @@
 package extension
 
 import (
-	"context"
 	"errors"
 
-	"github.com/ava-labs/avalanchego/database"
-	"github.com/ava-labs/avalanchego/database/versiondb"
-	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/network/p2p"
-	"github.com/ava-labs/avalanchego/snow/consensus/snowman"
-	avalanchecommon "github.com/ava-labs/avalanchego/snow/engine/common"
-
-	"github.com/ava-labs/avalanchego/snow/engine/snowman/block"
-	"github.com/prometheus/client_golang/prometheus"
-
-	"github.com/ava-labs/coreth/eth"
-	"github.com/ava-labs/coreth/params"
-	"github.com/ava-labs/coreth/plugin/evm/config"
 	"github.com/ava-labs/coreth/plugin/evm/message"
-	"github.com/ava-labs/coreth/plugin/evm/sync"
 	"github.com/ava-labs/coreth/sync/handlers"
-
-	"github.com/ava-labs/libevm/common"
-	"github.com/ava-labs/libevm/core/types"
 )
 
 var (
@@ -34,7 +16,7 @@ var (
 	errNilSyncSummaryProvider = errors.New("nil sync summary provider")
 	errNilSyncableParser      = errors.New("nil syncable parser")
 )
-k and syncer
+
 type LeafRequestConfig struct {
 	// LeafType is the type of the leaf node
 	LeafType message.NodeType
@@ -43,4 +25,3 @@ type LeafRequestConfig struct {
 	// Handler is the handler to use for the leaf request
 	Handler handlers.LeafRequestHandler
 }
-
