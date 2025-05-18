aaef8465e1fbdd3187169632f82e47a50d8c795c
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-05-18 23:37:44 +0300

    handle upstream licenses

--- core/blockchain_ext.go
+++ core/blockchain_ext.go
@@ -1,6 +1,5 @@
 // Copyright (C) 2019-2025, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
-// See the file LICENSE for licensing terms.
 package core
 
 import "github.com/ava-labs/libevm/metrics"
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -41,15 +41,77 @@ function test_golangci_lint {
 # TESTS='license_header' ADDLICENSE_FLAGS="--debug" ./scripts/lint.sh
 _addlicense_flags=${ADDLICENSE_FLAGS:-"--verify --debug"}
 function test_license_header {
-  local files=()
-  while IFS= read -r line; do files+=("$line"); done < <(find . -type f -name '*.go' ! -name '*.pb.go' ! -name 'mock_*.go' ! -name 'mocks_*.go' ! -name 'mocks.go' ! -name 'mock.go' ! -name 'gen_*.go' ! -path './**/*mock/*.go')
+  local upstream_licensed_folders_file="./scripts/upstream_licensed_folders.txt"
+  # Read the upstream_licensed_folders file into an array
+  mapfile -t upstream_licensed_folders <"$upstream_licensed_folders_file"
 
-  # shellcheck disable=SC2086
-  go run github.com/palantir/go-license@v1.25.0 \
-    --config=./license_header.yml \
-    ${_addlicense_flags} \
-    "${files[@]}"
+  local upstream_licensed_files=()
+  local default_files=()
+
+  # Shared find filters
+  local -a find_filters=(
+    -type f
+    -name '*.go'
+    ! -name '*.pb.go'
+    ! -name 'mock_*.go'
+    ! -name 'mocks_*.go'
+    ! -name 'mocks.go'
+    ! -name 'mock.go'
+    ! -name 'gen_*.go'
+    ! -path './**/*mock/*.go'
+  )
+
+  # Combined loop: build both upstream licensed find and exclude args
+  local -a upstream_licensed_find_args=()
+  local -a upstream_licensed_exclude_args=()
+  for line in "${upstream_licensed_folders[@]}"; do
+    if [[ "$line" == !* ]]; then
+      # Excluding files with ! only works for files, not directories
+      upstream_licensed_exclude_args+=(! -path "./${line:1}")
+    else
+      upstream_licensed_find_args+=(-path "./${line}" -o)
+    fi
+  done
+  # Remove the last '-o' from the arrays
+  unset 'upstream_licensed_find_args[${#upstream_licensed_find_args[@]}-1]'
+
+  echo "exclude: ${upstream_licensed_exclude_args[@]}"
+
+  # Find upstream_licensed files
+  mapfile -t upstream_licensed_files < <(
+    find . \
+      \( "${upstream_licensed_find_args[@]}" \) \
+      -a \( "${find_filters[@]}" \) \
+      "${upstream_licensed_exclude_args[@]}"
+  )
+
+  # Build exclusion args from upstream files
+  default_exclude_args=()
+  for f in "${upstream_licensed_files[@]}"; do
+    default_exclude_args+=(! -path "$f")
+  done
+
+  # Now find default files (exclude already licensed ones)
+  mapfile -t default_files < <(find . "${find_filters[@]}" "${default_exclude_args[@]}")
+
+  # Run license tool
+  if [[ ${#upstream_licensed_files[@]} -gt 0 ]]; then
+    echo "Running license tool on upstream_licensed files with header for upstream..."
+    go run github.com/palantir/go-license@v1.25.0 \
+      --config=./license_header_for_upstream.yml \
+      ${_addlicense_flags} \
+      "${upstream_licensed_files[@]}"
+  fi
+
+  if [[ ${#default_files[@]} -gt 0 ]]; then
+    echo "Running license tool on remaining files with default header..."
+    go run github.com/palantir/go-license@v1.25.0 \
+      --config=./license_header.yml \
+      ${_addlicense_flags} \
+      "${default_files[@]}"
+  fi
 }
+
 function run {
   local test="${1}"
   shift 1
