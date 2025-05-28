8d4fee666b869463bc98c61f8dae8362e4862577
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-05-29 01:36:03 +0300

    add more files to upstream check

--- metrics/prometheus/interfaces.go
+++ metrics/prometheus/interfaces.go
@@ -1,6 +1,6 @@
 // Copyright (C) 2019-2025, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
-// See the file LICENSE for licensing terms.
+
 package prometheus
 
 // Registry is a narrower interface of [prometheus.Registry] containing
