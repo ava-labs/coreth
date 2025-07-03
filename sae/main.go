1bcd4f0293ab13272e90d4dc28b95b29dcfe1eba
Stephen Buttolph <stephen@avalabs.org>
2025-07-03 10:41:32 -0400

    wip

--- sae/main.go
+++ sae/main.go
@@ -32,7 +32,11 @@ import (
 
 const targetAtomicTxsSize = 40 * units.KiB
 
-var _ hook.Points = &hooks{}
+var (
+	_ hook.Points = &hooks{}
+
+	errEmptyBlock = errors.New("empty block")
+)
 
 type hooks struct {
 	ctx         *snow.Context
@@ -58,83 +62,27 @@ func (h *hooks) ConstructBlock(
 	txs []*types.Transaction,
 	receipts []*types.Receipt,
 ) (*types.Block, error) {
-	cumulativeInputUTXOs, err := inputUTXOs(ancestors)
+	ancestorInputUTXOs, err := inputUTXOs(ancestors)
 	if err != nil {
 		return nil, err
 	}
 
-	var (
-		cumulativeSize    int
-		atomicTxs         []*atomic.Tx
-		batchContribution = big.NewInt(0)
-		gasUsed           gas.Gas
+	atomicTxs, batchContribution, err := packAtomicTxs(
+		ctx,
+		state,
+		h.ctx.AVAXAssetID,
+		header.BaseFee,
+		ancestorInputUTXOs,
+		h.mempool,
 	)
-	for {
-		tx, exists := h.mempool.NextTx()
-		if !exists {
-			break
-		}
+	if err != nil {
+		return nil, err
+	}
 
-		// Ensure that adding [tx] to the block will not exceed the block size
-		// soft limit.
-		txSize := len(tx.SignedBytes())
-		if cumulativeSize+txSize > targetAtomicTxsSize {
-			h.mempool.CancelCurrentTx(tx.ID())
-			break
-		}
-
-		inputUTXOs := tx.InputUTXOs()
-		if cumulativeInputUTXOs.Overlaps(inputUTXOs) {
-			// Discard the transaction from the mempool since it will fail
-			// verification after this block has been accepted.
-			//
-			// Note: if the proposed block is not accepted, the transaction may
-			// still be valid, but we discard it early here based on the
-			// assumption that the proposed block will most likely be accepted.
-			log.Debug("discarding tx due to overlapping input utxos", "txID", tx.ID())
-			h.mempool.DiscardCurrentTx(tx.ID())
-			continue
-		}
-
-		var (
-			txGasUsed, txContribution *big.Int
-			err                       error
-		)
-
-		// Note: we do not need to check if we are in at least ApricotPhase4
-		// here because we assume that this function will only be called when
-		// the block is in at least ApricotPhase5.
-		txContribution, txGasUsed, err = tx.BlockFeeContribution(true, h.ctx.AVAXAssetID, header.BaseFee)
-		if err != nil {
-			return nil, err
-		}
-
-		// TODO: Make this correctly
-		op := hook.Op{
-			Gas: gas.Gas(txGasUsed.Uint64()),
-		}
-		err = state.Apply(op)
-		if errors.Is(err, worstcase.ErrBlockTooFull) || errors.Is(err, worstcase.ErrQueueTooFull) {
-			// Send [tx] back to the mempool's tx heap.
-			h.mempool.CancelCurrentTx(tx.ID())
-			break
-		}
-		if err != nil {
-			log.Debug("discarding tx from mempool due to failed verification",
-				"txID", tx.ID(),
-				"err", err,
-			)
-			h.mempool.DiscardCurrentTx(tx.ID())
-			continue
-		}
-
-		atomicTxs = append(atomicTxs, tx)
-		cumulativeInputUTXOs.Union(inputUTXOs)
-
-		// Add the [txGasUsed] to the [batchGasUsed] when the [tx] has passed verification
-		gasUsed += gas.Gas(txGasUsed.Uint64())
-		batchContribution.Add(batchContribution, txContribution)
-		cumulativeSize += txSize
+	// Blocks must either settle a prior transaction, include a new ethereum tx,
+	// or include a new atomic tx.
+	if header.GasUsed == 0 && len(txs) == 0 && len(atomicTxs) == 0 {
+		return nil, errEmptyBlock
 	}
 
 	// If there is a non-zero number of transactions, marshal them and return the byte slice
@@ -155,8 +103,7 @@ func (h *hooks) ConstructBlock(
 	// transactions to be included, then the block is empty and should be
 	// considered invalid.
 	if len(txs) == 0 {
-		// this could happen due to the async logic of geth tx pool
-		return nil, nil, nil, atomicvm.ErrEmptyBlock
+		return nil, errEmptyBlock
 	}
 
 	// If there are no atomic transactions, but there is a non-zero number of regular transactions, then
@@ -195,6 +142,91 @@ func inputUTXOs(blocks iter.Seq[*types.Block]) (set.Set[ids.ID], error) {
 		}
 	}
 	return inputUTXOs, nil
+}
+
+func packAtomicTxs(
+	ctx context.Context,
+	state hook.State,
+	avaxAssetID ids.ID,
+	baseFee *big.Int,
+	ancestorInputUTXOs set.Set[ids.ID],
+	mempool *atomictxpool.Mempool,
+) ([]*atomic.Tx, *big.Int, error) {
+	var (
+		cumulativeSize    int
+		atomicTxs         []*atomic.Tx
+		batchContribution = big.NewInt(0)
+	)
+	for {
+		tx, exists := mempool.NextTx()
+		if !exists {
+			break
+		}
+
+		// Ensure that adding [tx] to the block will not exceed the block size
+		// soft limit.
+		txSize := len(tx.SignedBytes())
+		if cumulativeSize+txSize > targetAtomicTxsSize {
+			mempool.CancelCurrentTx(tx.ID())
+			break
+		}
+
+		inputUTXOs := tx.InputUTXOs()
+		if ancestorInputUTXOs.Overlaps(inputUTXOs) {
+			// Discard the transaction from the mempool since it will fail
+			// verification after this block has been accepted.
+			//
+			// Note: if the proposed block is not accepted, the transaction may
+			// still be valid, but we discard it early here based on the
+			// assumption that the proposed block will most likely be accepted.
+			log.Debug("discarding tx due to overlapping input utxos",
+				"txID", tx.ID(),
+			)
+			mempool.DiscardCurrentTx(tx.ID())
+			continue
+		}
+
+		// Note: we do not need to check if we are in at least ApricotPhase4
+		// here because we assume that this function will only be called when
+		// the block is in at least ApricotPhase5.
+		txContribution, txGasUsed, err := tx.BlockFeeContribution(true, avaxAssetID, baseFee)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// TODO: Make this correctly
+		op := hook.Op{
+			Gas: gas.Gas(txGasUsed.Uint64()),
+		}
+		err = state.Apply(op)
+		if errors.Is(err, worstcase.ErrBlockTooFull) || errors.Is(err, worstcase.ErrQueueTooFull) {
+			// Send [tx] back to the mempool's tx heap.
+			mempool.CancelCurrentTx(tx.ID())
+			break
+		}
+		if err != nil {
+			log.Debug("discarding tx from mempool due to failed verification",
+				"txID", tx.ID(),
+				"err", err,
+			)
+			mempool.DiscardCurrentTx(tx.ID())
+			continue
+		}
+
+		atomicTxs = append(atomicTxs, tx)
+		ancestorInputUTXOs.Union(inputUTXOs)
+
+		batchContribution.Add(batchContribution, txContribution)
+		cumulativeSize += txSize
+	}
+	return atomicTxs, batchContribution, nil
+}
+
+func marshalAtomicTxs(txs []*atomic.Tx) ([]byte, error) {
+	if len(txs) == 0 {
+		return nil, nil
+	}
+	return atomic.Codec.Marshal(atomic.CodecVersion, txs)
 }
 
 func main() {
