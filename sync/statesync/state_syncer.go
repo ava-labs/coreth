5ecd01cb85d6953c3b87dce1610056efbe6231bc
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2025-07-31 15:20:13 +0300

    chore(sync/statesync): add new syncer config validation
    - Add Validate() method on the Config struct used for constructing the
      state trie syncer.
    - Add extensive Config validation tests.
    
    resolves #1084
    
    Signed-off-by: Tsvetan Dimitrov (tsvetan.dimitrov@avalabs.org)

--- sync/statesync/state_syncer.go
+++ sync/statesync/state_syncer.go
@@ -5,6 +5,7 @@ package statesync
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"sync"
 
@@ -24,7 +25,17 @@ const (
 	defaultNumWorkers      = 8
 )
 
-var _ synccommon.Syncer = (*stateSync)(nil)
+var (
+	_ synccommon.Syncer = (*stateSync)(nil)
+
+	errNilClient                       = errors.New("Client cannot be nil")
+	errNilDatabase                     = errors.New("DB cannot be nil")
+	errEmptyRoot                       = errors.New("Root cannot be empty")
+	errInvalidBatchSize                = errors.New("BatchSize must be greater than 0")
+	errInvalidMaxOutstandingCodeHashes = errors.New("MaxOutstandingCodeHashes must be greater than 0")
+	errInvalidNumCodeFetchingWorkers   = errors.New("NumCodeFetchingWorkers must be greater than 0")
+	errInvalidRequestSize              = errors.New("RequestSize must be greater than 0")
+)
 
 type Config struct {
 	Root                     common.Hash
@@ -34,6 +45,33 @@ type Config struct {
 	MaxOutstandingCodeHashes int    // Maximum number of code hashes in the code syncer queue
 	NumCodeFetchingWorkers   int    // Number of code syncing threads
 	RequestSize              uint16 // Number of leafs to request from a peer at a time
+}
+
+// Validate checks if the configuration is valid and returns an error if not.
+func (c *Config) Validate() error {
+	if c.Client == nil {
+		return errNilClient
+	}
+	if c.DB == nil {
+		return errNilDatabase
+	}
+	if c.Root == (common.Hash{}) {
+		return errEmptyRoot
+	}
+	if c.BatchSize <= 0 {
+		return errInvalidBatchSize
+	}
+	if c.MaxOutstandingCodeHashes <= 0 {
+		return errInvalidMaxOutstandingCodeHashes
+	}
+	if c.NumCodeFetchingWorkers <= 0 {
+		return errInvalidNumCodeFetchingWorkers
+	}
+	if c.RequestSize == 0 {
+		return errInvalidRequestSize
+	}
+
+	return nil
 }
 
 // stateSync keeps the state of the entire state sync operation.
