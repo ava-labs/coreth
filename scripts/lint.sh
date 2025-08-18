4dceaa17bffcc3e25d9a2ace52f125b443d5c22a
Austin Larson <austin.larson@avalabs.org>
2025-08-18 13:23:22 -0400

    style: naming

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -21,7 +21,7 @@ grep -P 'lint.sh' scripts/lint.sh &>/dev/null || (
 # Read excluded directories into arrays
 DEFAULT_FILES=()
 UPSTREAM_FILES=()
-EXTRA_LINT_FILE=""
+AVALANCHE_LINT_FILE=""
 function read_dirs {
   local upstream_folders_file="./scripts/upstream_files.txt"
   # Read the upstream_folders file into an array
@@ -74,33 +74,35 @@ function read_dirs {
   # Now find default files (exclude already licensed ones)
   mapfile -t DEFAULT_FILES < <(find . "${find_filters[@]}" "${default_exclude_args[@]}")
 
-  # append exclusions to .extra-golangci.yml in temp file
-  EXTRA_LINT_FILE="$(mktemp -d)/.extra-golangci.yml"
-  echo "Extra lint file at: $EXTRA_LINT_FILE"
-  cp .extra-golangci.yml "$EXTRA_LINT_FILE"
-  echo "    paths-except:" >> "$EXTRA_LINT_FILE"
+  # copy avalanche file to temp directory to edit
+  AVALANCHE_LINT_FILE="$(mktemp -d)/.avalanche-golangci.yml"
+  echo "Avalanche lint file at: $AVALANCHE_LINT_FILE"
+  cp .avalanche-golangci.yml "$AVALANCHE_LINT_FILE"
+
+  # Exclude all upstream files dynamically
+  echo "    paths-except:" >> "$AVALANCHE_LINT_FILE"
   for f in "${UPSTREAM_FILES[@]}"; do
     # exclude pre-pended "./"
-    echo "      - \"${f:2}\$\"" >> "$EXTRA_LINT_FILE"
+    echo "      - \"${f:2}\$\"" >> "$AVALANCHE_LINT_FILE"
   done
 }
 
 # by default, "./scripts/lint.sh" runs all lint tests
 # to run only "license_header" test
 # TESTS='license_header' ./scripts/lint.sh
-TESTS=${TESTS:-"golangci_lint extra_golangci_lint license_header require_error_is_no_funcs_as_params single_import interface_compliance_nil require_no_error_inline_func import_testing_only_in_tests"}
+TESTS=${TESTS:-"golangci_lint avalanche_golangci_lint license_header require_error_is_no_funcs_as_params single_import interface_compliance_nil require_no_error_inline_func import_testing_only_in_tests"}
 
 function test_golangci_lint {
   go run github.com/golangci/golangci-lint/cmd/golangci-lint@v1.63 run --config .golangci.yml
 }
 
-function test_extra_golangci_lint {
-  if [[ ! -f $EXTRA_LINT_FILE ]]; then
+function test_avalanche_golangci_lint {
+  if [[ ! -f $AVALANCHE_LINT_FILE ]]; then
     return 0
   fi
 
   go run github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.1.6 run \
-  --config "$EXTRA_LINT_FILE" \
+  --config "$AVALANCHE_LINT_FILE" \
   || return 1
 }
 
