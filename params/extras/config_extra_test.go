3acffeb3cd0efd01fe5bbb2bd40fd6e554ac1aeb
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-09-04 16:45:56 -0400

    add unit tests

--- params/extras/config_extra_test.go
+++ params/extras/config_extra_test.go
@@ -7,7 +7,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	"github.com/ava-labs/coreth/precompile/contracts/warp"
 	"github.com/ava-labs/coreth/utils"
 )
 
@@ -123,6 +125,131 @@ func TestIsForkTransition(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			res := IsForkTransition(test.fork, test.parent, test.current)
 			assert.Equal(t, test.transitioned, res)
+		})
+	}
+}
+
+func TestCheckPrecompilesCompatible(t *testing.T) {
+	type test struct {
+		name                string
+		storedUpgrades      []PrecompileUpgrade
+		newUpgrades         []PrecompileUpgrade
+		headTimestamp       uint64
+		wantErr             bool
+		expectedErrContains string
+	}
+
+	tests := []test{
+		{
+			name:           "no upgrades to no upgrades - should succeed",
+			storedUpgrades: []PrecompileUpgrade{},
+			newUpgrades:    []PrecompileUpgrade{},
+			headTimestamp:  100,
+			wantErr:        false,
+		},
+		{
+			name:           "adding upgrades to empty config - should succeed (new logic)",
+			storedUpgrades: []PrecompileUpgrade{},
+			newUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(50), 0, false)},
+			},
+			headTimestamp: 100, // Past the activation time, but should still succeed
+			wantErr:       false,
+		},
+		{
+			name:           "adding multiple upgrades to empty config - should succeed",
+			storedUpgrades: []PrecompileUpgrade{},
+			newUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(50), 0, false)},
+				{Config: warp.NewConfig(utils.NewUint64(80), 0, true)}, // disable at 80
+			},
+			headTimestamp: 100,
+			wantErr:       false,
+		},
+		{
+			name: "adding upgrades to existing config - should fail if retroactive",
+			storedUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(30), 0, false)},
+			},
+			newUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(30), 0, false)}, // same as stored
+				{Config: warp.NewConfig(utils.NewUint64(50), 0, true)},  // new retroactive upgrade
+			},
+			headTimestamp:       100,
+			wantErr:             true,
+			expectedErrContains: "cannot retroactively enable PrecompileUpgrade[1]",
+		},
+		{
+			name: "modifying existing activated upgrade - should fail",
+			storedUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(50), 0, false)},
+			},
+			newUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(50), 1, false)}, // different admin address
+			},
+			headTimestamp:       100,
+			wantErr:             true,
+			expectedErrContains: "PrecompileUpgrade[0]",
+		},
+		{
+			name: "removing existing activated upgrade - should fail",
+			storedUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(50), 0, false)},
+			},
+			newUpgrades:         []PrecompileUpgrade{}, // removed the upgrade
+			headTimestamp:       100,
+			wantErr:             true,
+			expectedErrContains: "missing PrecompileUpgrade[0]",
+		},
+		{
+			name: "adding non-retroactive upgrades to existing config - should succeed",
+			storedUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(50), 0, false)},
+			},
+			newUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(50), 0, false)}, // same as stored
+				{Config: warp.NewConfig(utils.NewUint64(150), 0, true)}, // future upgrade
+			},
+			headTimestamp: 100,
+			wantErr:       false,
+		},
+		{
+			name: "identical configs - should succeed",
+			storedUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(50), 0, false)},
+				{Config: warp.NewConfig(utils.NewUint64(80), 0, true)},
+			},
+			newUpgrades: []PrecompileUpgrade{
+				{Config: warp.NewConfig(utils.NewUint64(50), 0, false)},
+				{Config: warp.NewConfig(utils.NewUint64(80), 0, true)},
+			},
+			headTimestamp: 100,
+			wantErr:       false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create stored config with the stored upgrades
+			storedConfig := &ChainConfig{
+				UpgradeConfig: UpgradeConfig{
+					PrecompileUpgrades: tt.storedUpgrades,
+				},
+			}
+
+			// Test the compatibility check
+			err := storedConfig.checkPrecompilesCompatible(tt.newUpgrades, tt.headTimestamp)
+
+			if tt.wantErr {
+				require.Error(t, err, "Expected error for test case: %s", tt.name)
+				if tt.expectedErrContains != "" {
+					assert.Contains(t, err.Error(), tt.expectedErrContains, "Error message should contain expected text")
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Expected no error for test case %s, but got: %v", tt.name, err)
+				}
+			}
 		})
 	}
 }
