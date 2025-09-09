8b778d54c2a4d012c953502bf58c071afb752f61
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-09-09 16:18:59 -0400

    Update core/blockchain_ext_test.go
    Co-authored-by: Stephen Buttolph <stephen@avalabs.org>
    Signed-off-by: Jonathan Oppenheimer <147infiniti@gmail.com>

--- core/blockchain_ext_test.go
+++ core/blockchain_ext_test.go
@@ -1822,7 +1822,7 @@ func ReexecMaxBlocksTest(t *testing.T, create ReexecTestFunc) {
 	}
 	newChain, restartedChain := checkBlockChainState(t, blockchain, gspec, chainDB, checkCreate, checkState)
 
-	foundTxs = append(foundTxs, missingTxs...)
+	allTxs := slices.Concat(foundTxs, missingTxs)
 	for _, bc := range []*BlockChain{newChain, restartedChain} {
 		// We should confirm that snapshots were properly initialized
 		if bc.snaps == nil && bc.cacheConfig.SnapshotLimit > 0 {
