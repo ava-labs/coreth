31a426674c597d3172d2d5c2e05d1df5fa2dfc26
Draco <draco@dracoli.com>
2025-09-21 19:25:22 -0400

    feat: add block database wrapper for eth database

--- plugin/evm/config/config.go
+++ plugin/evm/config/config.go
@@ -138,7 +138,9 @@ type Config struct {
 	StateSyncRequestSize     uint16 `json:"state-sync-request-size"`
 
 	// Database Settings
-	InspectDatabase bool `json:"inspect-database"` // Inspects the database on startup if enabled.
+	InspectDatabase         bool `json:"inspect-database"`            // Inspects the database on startup if enabled.
+	BlockDatabaseEnabled    bool `json:"block-database-enabled"`      // Use block database for storing block data
+	BlockDatabaseSyncToDisk bool `json:"block-database-sync-to-disk"` // Sync block database to disk
 
 	// SkipUpgradeCheck disables checking that upgrades must take place before the last
 	// accepted block. Skipping this check is useful when a node operator does not update
