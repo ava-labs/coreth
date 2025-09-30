2d60e6e492022f71046cbc2270f1e418cd971092
Arran Schlosberg <me@arranschlosberg.com>
2025-09-30 18:07:38 +0100

    feat!: explicit `customtypes.Register()`

--- plugin/evm/customtypes/libevm.go
+++ plugin/evm/customtypes/libevm.go
@@ -7,8 +7,16 @@ import (
 	ethtypes "github.com/ava-labs/libevm/core/types"
 )
 
-var extras = ethtypes.RegisterExtras[
-	HeaderExtra, *HeaderExtra,
-	BlockBodyExtra, *BlockBodyExtra,
-	isMultiCoin,
-]()
+var extras ethtypes.ExtraPayloads[*HeaderExtra, *BlockBodyExtra, isMultiCoin]
+
+// Register registers the types with libevm. It MUST NOT be called more than
+// once and therefore is only allowed to be used in tests and `package main`, to
+// avoid polluting other packages that transitively depend on this one but don't
+// need registration.
+func Register() {
+	extras = ethtypes.RegisterExtras[
+		HeaderExtra, *HeaderExtra,
+		BlockBodyExtra, *BlockBodyExtra,
+		isMultiCoin,
+	]()
+}
