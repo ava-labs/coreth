99519a5e4c7cc3c241a184484c3c8f42d2fef338
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-10-04 20:05:28 +0300

    remove legacy aggregator tests

--- tests/warp/aggregator/mocks_generate_test.go
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2025, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-package aggregator
-
-//go:generate go run go.uber.org/mock/mockgen -package=$GOPACKAGE -destination=mock_signature_getter.go . SignatureGetter
