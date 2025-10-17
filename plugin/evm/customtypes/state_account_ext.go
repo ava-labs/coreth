ca0bc14757f4c2eaa59165ca73a4b6ebb7d9c8d7
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-10-17 15:28:28 -0400

    lint

--- plugin/evm/customtypes/state_account_ext.go
+++ plugin/evm/customtypes/state_account_ext.go
@@ -1,8 +1,9 @@
+// Copyright (C) 2019-2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
 package customtypes
 
-import (
-	ethtypes "github.com/ava-labs/libevm/core/types"
-)
+import ethtypes "github.com/ava-labs/libevm/core/types"
 
 type isMultiCoin bool
 
