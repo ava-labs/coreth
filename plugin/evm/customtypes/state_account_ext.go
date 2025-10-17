65b6bf9004c09a624b5ae6608f6d338e2517bdc9
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-10-17 15:23:13 -0400

    remove stateaccount

--- plugin/evm/customtypes/state_account_ext.go
+++ plugin/evm/customtypes/state_account_ext.go
@@ -1,17 +1,12 @@
-// Copyright (C) 2019-2025, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
 package customtypes
 
 import (
-	"github.com/ava-labs/libevm/libevm/pseudo"
-
 	ethtypes "github.com/ava-labs/libevm/core/types"
 )
 
 type isMultiCoin bool
 
-var IsMultiCoinPayloads pseudo.Accessor[ethtypes.StateOrSlimAccount, isMultiCoin]
+var IsMultiCoinPayloads = extras.StateAccount
 
 func IsMultiCoin(s ethtypes.StateOrSlimAccount) bool {
 	return bool(IsMultiCoinPayloads.Get(s))
